37b20c5d9c5494a8bf41a2dbaa34807e6ffafe86
Temeka <misstemeka1@live.com>
2018-09-11 21:37:56 -0500

    Day(s) Counter

--- DayCounter/Program.cs
+++ DayCounter/Program.cs
@@ -6,7 +6,27 @@ namespace DayCounter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Day Counter!");
+
+            
+            Console.WriteLine("Please enter any date (i.e mm/dd/yyy): ");
+            string FirstDate = Console.ReadLine();
+
+            //parsing the input to the string
+            DateTime DateOne = DateTime.Parse(FirstDate);
+
+            Console.WriteLine("Please enter your second date:");
+            string SecDate = Console.ReadLine();
+
+            DateTime DateTwo = DateTime.Parse(SecDate);
+
+            //declaring the time span
+            TimeSpan Count = DateOne - DateTwo;
+
+            //prompting the compiler to print result. Works whether adding or subtracting dates.
+            Console.WriteLine("There were " + Count.TotalDays + "days between the dates");
+
+            Console.ReadLine();
         }
     }
 }
